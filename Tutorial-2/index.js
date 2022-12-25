9052f0501c6d3990e86c0a75667dce0c5bf7e087
Abir Hasan <cse.170201063@gmail.com>
2022-12-26 04:41:44 +0600

    tutorial 3 / reducer

--- Tutorial-2/index.js
+++ Tutorial-2/index.js
@@ -32,3 +32,23 @@ const addUser = () => {
         payload: {name:"shakil"}
     }
 }
+
+// reducer for counter
+const counterReducer = (state=initialCounterState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count+1
+            }
+            
+        case DECREMENT:
+            return {
+              ...state,
+              count: state.count - 1,
+            };
+
+        default:
+            state;
+    }
+}
